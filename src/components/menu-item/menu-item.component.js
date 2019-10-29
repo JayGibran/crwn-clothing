2bf7e624b00799290f55c131a62df0ea12e99b2d
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-10-29 18:05:23 +0000

    moved our directory and shop data into its own reducer, create respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import './menu-item.style.scss'
+import './menu-item.styles.scss'
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl }) =>(
   <div className={`${size} menu-item`} onClick={() => history.push(linkUrl)}>
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,24 +1,16 @@
-import React, {Component} from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-class Shop extends Component{
-  
-  constructor(props){
-    super(props);
-    this.state = {
-       collections : SHOP_DATA
-    }
-  }
+import CollectionsOverview 
+from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component'
 
-  render(){
-    const {collections} = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(collection => (<CollectionPreview id={collection.id} {...collection} />))}
-      </div>)  
-  }
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> 
+  </div>
+);
 
-}
 
-export default Shop;
+export default ShopPage;
