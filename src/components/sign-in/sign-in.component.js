351c7758fcb2964596c0380be2ae4146dcaf3d52
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-11-18 17:07:45 +0000

    updated our application App component, sign-in component and sign-up component to use hooks

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
@@ -11,69 +11,64 @@ import {
 
 import "./sign-in.style.scss";
 
-class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
+const SignIn = ({ emailSignInStart, googleSIgnInStart }) => {
+  const [useCredentials, setCredentials] = useState({
+    email: "",
+    password: ""
+  });
 
-  handleSubmit = async event => {
+  const handleSubmit = async event => {
     event.preventDefault();
 
-    const { email, password } = this.state;
-    const { emailSignInStart } = this.props;
+    const { email, password } = useCredentials;
 
     emailSignInStart(email, password);
   };
 
-  handleChange = event => {
+  const handleChange = event => {
     const { value, name } = event.target;
-    this.setState({ [name]: value });
+    setCredentials({
+      ...useCredentials,
+      [name]: value
+    });
   };
 
-  render() {
-    const { googleSIgnInStart } = this.props;
+  return (
+    <div className="sign-in">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
 
-    return (
-      <div className="sign-in">
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
-
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name="email"
-            label="email"
-            type="email"
-            value={this.state.email}
-            handleChange={this.handleChange}
-            required
-          />
-          <FormInput
-            name="password"
-            label="password"
-            type="password"
-            value={this.state.password}
-            handleChange={this.handleChange}
-            required
-          />
-          <div className="buttons">
-            <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton
-              type="button"
-              onClick={googleSIgnInStart}
-              isGoogleSignIn
-            >
-              Sign In with Google
-            </CustomButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          name="email"
+          label="email"
+          type="email"
+          value={useCredentials.email}
+          handleChange={handleChange}
+          required
+        />
+        <FormInput
+          name="password"
+          label="password"
+          type="password"
+          value={useCredentials.password}
+          handleChange={handleChange}
+          required
+        />
+        <div className="buttons">
+          <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton
+            type="button"
+            onClick={googleSIgnInStart}
+            isGoogleSignIn
+          >
+            Sign In with Google
+          </CustomButton>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   googleSIgnInStart: () => dispatch(googleSIgnInStart()),
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,47 +1,35 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { useEffect } from "react";
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import CollectionsOverviewContainer 
-from '../../components/collections-overview/collection-overview.container';
+import CollectionsOverviewContainer from "../../components/collections-overview/collection-overview.container";
 
-import CollectionContainer from '../../pages/collection/collection.container';
+import CollectionContainer from "../../pages/collection/collection.container";
 
-import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-class ShopPage extends React.Component {
-  
-  state = {
-    loading: true
-  }
-  
-  unsubscribeFromSnapshot = null;
-
-  componentDidMount(){
-    const {fetchCollectionsStartAsync} = this.props;
+const ShopPage = ({ fetchCollectionsStartAsync, match }) => {
+  useEffect(() => {
     fetchCollectionsStartAsync();
-  }  
+  }, [fetchCollectionsStartAsync]);
 
-  render(){
-    
-    const { match } = this.props;
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionContainer}
+      />
+    </div>
+  );
+};
 
-    return(
-      <div className='shop-page'>
-        <Route exact path={`${match.path}`} 
-               component={CollectionsOverviewContainer} 
-        />
-        <Route path={`${match.path}/:collectionId`} 
-               component={CollectionContainer} 
-        /> 
-      </div>
-    );
-  }
-}
-
-
-const mapDispatchToProps = dispatch  => ({
-  fetchCollectionsStartAsync: collections => dispatch(fetchCollectionsStart()) 
-})
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync: collections => dispatch(fetchCollectionsStart())
+});
 
 export default connect(null, mapDispatchToProps)(ShopPage);
